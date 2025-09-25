f0a1e00adabf58600ee650157d4ec72bed9713f2
Maxim Danilov <abl1v1on@yandex.ru>
2025-09-25 17:05:34 +0500

    delete numeric str custom type

--- src/api/auth/schemas.py
+++ src/api/auth/schemas.py
@@ -1,6 +1,5 @@
 from enum import Enum
 from typing import Annotated
-from pydantic.validators import str_validator
 from pydantic_core import PydanticCustomError
 from annotated_types import Len
 from pydantic import BaseModel, EmailStr
@@ -11,29 +10,13 @@ class JWTType(str, Enum):
     REFRESH = "refresh"
 
 
-class NumericStr(str):
-    @classmethod
-    def __get_pydantic_validators__(cls):
-        yield str_validator
-        yield cls.validate
-
-    @classmethod
-    def validate(cls, value: str) -> str:
-        if not value.isdigit():
-            raise PydanticCustomError(
-                "numeric_str",
-                "value must be a string of numbers",
-            )
-        return value
-
-
 class AuthCredentialsSchema(BaseModel):
     username: Annotated[str, Len(min_length=3, max_length=60)]
     password: Annotated[str, Len(min_length=8, max_length=50)]
 
 
 class PayloadSchema(BaseModel):
-    sub: NumericStr
+    sub: str
     email: EmailStr
     is_activated: bool
     typ: JWTType
