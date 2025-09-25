0f60acebc17aa242ce00fbcdc291556f5a45b707
Maxim Danilov <abl1v1on@yandex.ru>
2025-09-25 21:29:39 +0500

    add cors settings

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from core import settings
 from api import main_api_router
@@ -7,6 +8,13 @@ from api import main_api_router
 
 app = FastAPI()
 app.include_router(main_api_router)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 if __name__ == "__main__":
