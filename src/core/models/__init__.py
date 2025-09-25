62c8417410409e6c9853e1f40a6996b7f4f56dda
Maxim Danilov <abl1v1on@yandex.ru>
2025-09-25 14:45:00 +0500

    create database helper

--- src/core/models/__init__.py
+++ src/core/models/__init__.py
@@ -1,5 +1,8 @@
 __all__ = [
     "Base",
+    "helper",
+    "session_dep",
 ]
 
 from .base import Base
+from .db_helper import helper, session_dep
---
+++ src/core/models/db_helper.py
@@ -0,0 +1,25 @@
+from fastapi import Depends
+from typing import AsyncGenerator, Annotated
+from sqlalchemy.ext.asyncio import (
+    AsyncSession,
+    create_async_engine,
+    async_sessionmaker,
+)
+
+from core import settings
+
+
+class DBHelper:
+    def __init__(self) -> None:
+        self.engine = create_async_engine(url=settings.db.url, echo=settings.db.echo)
+        self.session_factory = async_sessionmaker(
+            bind=self.engine, autoflush=False, autocommit=True, expire_on_commit=False
+        )
+
+    async def session_dependency(self) -> AsyncGenerator[AsyncSession, None]:
+        async with self.session_factory() as session:
+            yield session
+
+
+helper = DBHelper()
+session_dep = Annotated[AsyncSession, Depends(helper.session_dependency)]
