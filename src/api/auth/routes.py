fa13ce3c1aed8f05a78042710ba47ca789f0817c
Maxim Danilov <abl1v1on@yandex.ru>
2025-09-25 19:06:13 +0500

    create /refresh route

--- src/api/auth/routes.py
+++ src/api/auth/routes.py
@@ -1,10 +1,16 @@
-from fastapi import APIRouter
+from typing import Annotated
+from fastapi import APIRouter, Depends, HTTPException
 
+from core.models import User
 from .service import service_dep
 from .schemas import (
     AuthCredentialsSchema,
     AccessTokenSchema,
+    PayloadSchema,
+    JWTType,
 )
+from . import dependencies
+from . import exc
 
 
 router = APIRouter(prefix="/auth", tags=["Аутентификация"])
@@ -16,3 +22,11 @@ async def handle_login_user(
     credentials: AuthCredentialsSchema,
 ):
     return await service.auth_user(credentials=credentials)
+
+
+@router.get("/refresh", response_model=AccessTokenSchema)
+async def handle_refresh_access_token(
+    service: service_dep,
+    payload: Annotated[PayloadSchema, Depends(dependencies.payload_dependency)],
+):
+    return await service.refresh_token(payload)
