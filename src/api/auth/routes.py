7ac8b28906a1bfbf4ac3a436941c4ed564502e4e
Maxim Danilov <abl1v1on@yandex.ru>
2025-09-25 17:32:40 +0500

    move auth user logic to auth api service

--- src/api/auth/routes.py
+++ src/api/auth/routes.py
@@ -1,13 +1,10 @@
-from fastapi import APIRouter, HTTPException, status
+from fastapi import APIRouter
 
 from .service import service_dep
 from .schemas import (
     AuthCredentialsSchema,
     AccessTokenSchema,
-    PayloadSchema,
-    JWTType,
 )
-from api.users.service import service_dep as users_service_dep
 
 
 router = APIRouter(prefix="/auth", tags=["Аутентификация"])
@@ -15,35 +12,7 @@ router = APIRouter(prefix="/auth", tags=["Аутентификация"])
 
 @router.post("/login", response_model=AccessTokenSchema)
 async def handle_login_user(
-    auth_service: service_dep,
-    users_service: users_service_dep,
+    service: service_dep,
     credentials: AuthCredentialsSchema,
 ):
-    user = await users_service.get_user_by(
-        need_raise_exception=False,
-        username=credentials.username,
-    )
-
-    if not user or not auth_service.verify(credentials.password, user.password):
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="invalid username or password",
-        )
-
-    access_token_payload = PayloadSchema(
-        sub=str(user.id),  # type: ignore
-        email=user.email,  # type: ignore
-        is_activated=user.is_activated,
-        typ=JWTType.ACCESS,
-    )
-    access_token = auth_service.encode_jwt(access_token_payload)
-
-    refresh_token_payload = PayloadSchema(
-        sub=str(user.id),  # type: ignore
-        email=user.email,  # type: ignore
-        is_activated=user.is_activated,
-        typ=JWTType.REFRESH,
-    )
-    refresh_token = auth_service.encode_jwt(refresh_token_payload)
-
-    return AccessTokenSchema(access_token=access_token, refresh_token=refresh_token)
+    return await service.auth_user(credentials=credentials)
