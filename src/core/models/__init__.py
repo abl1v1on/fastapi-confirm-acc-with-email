b19d5490bc553440a8fa9f4d9b21368f0782a22b
Maxim Danilov <abl1v1on@yandex.ru>
2025-09-25 14:52:54 +0500

    create users sqla model

--- src/core/models/__init__.py
+++ src/core/models/__init__.py
@@ -2,7 +2,9 @@ __all__ = [
     "Base",
     "helper",
     "session_dep",
+    "User",
 ]
 
 from .base import Base
 from .db_helper import helper, session_dep
+from .users import User
---
+++ src/core/models/users.py
@@ -0,0 +1,34 @@
+from datetime import datetime, timezone
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import (
+    String,
+    CheckConstraint,
+    UniqueConstraint,
+    DateTime,
+    func,
+)
+
+from .base import Base
+
+
+class User(Base):
+    username: Mapped[str] = mapped_column(String(60), unique=True)
+    email: Mapped[str] = mapped_column(String(256), unique=True)
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        default=lambda: datetime.now(timezone.utc),
+        server_default=func.now(),
+    )
+
+    __table_args__ = (
+        UniqueConstraint("username", name="uq_user_username"),
+        UniqueConstraint("email", name="uq_user_email"),
+        CheckConstraint(
+            "LENGTH(username) >= 3",
+            name="ck_user_username_len_ge_3",
+        ),
+        CheckConstraint(
+            "LENGTH(email) >= 6",
+            name="ck_user_email_len_ge_6",
+        ),
+    )
