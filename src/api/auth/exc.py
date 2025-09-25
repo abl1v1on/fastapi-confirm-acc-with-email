16698b2d23dbb35952d7a6096d91fb452c452ce9
Maxim Danilov <abl1v1on@yandex.ru>
2025-09-25 17:49:46 +0500

    create custom exceptions for auth app

---
+++ src/api/auth/exc.py
@@ -0,0 +1,25 @@
+from fastapi import HTTPException, status
+
+
+class InvalidCredentialsException(HTTPException):
+    def __init__(self) -> None:
+        super().__init__(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="invalid username or password",
+        )
+
+
+class TokenHasExpiredException(HTTPException):
+    def __init__(self) -> None:
+        super().__init__(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="token has expired",
+        )
+
+
+class TokenDecodeException(HTTPException):
+    def __init__(self) -> None:
+        super().__init__(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="token decode error",
+        )
