3fde1741ae3bb69dd360e7690742cf49cf7bc72f
Maxim Danilov <abl1v1on@yandex.ru>
2025-09-27 16:06:54 +0500

    add routes for user activation

--- src/api/auth/routes.py
+++ src/api/auth/routes.py
@@ -1,11 +1,13 @@
 from typing import Annotated
 from fastapi import APIRouter, Depends
 
-from .service import service_dep
+from core.models import User
+from .service import service_dep, email_service_dep
 from .schemas import (
     AuthCredentialsSchema,
     AccessTokenSchema,
     PayloadSchema,
+    ConfirmCodeSchema,
 )
 from . import dependencies as deps
 
@@ -24,6 +26,23 @@ async def handle_login_user(
 @router.get("/refresh", response_model=AccessTokenSchema)
 async def handle_refresh_access_token(
     service: service_dep,
+    user: Annotated[User, Depends(deps.user_dependency)],
     payload: Annotated[PayloadSchema, Depends(deps.payload_dependency)],
 ):
-    return await service.refresh_token(payload)
+    return await service.refresh_token(user, payload)
+
+
+@router.get("/get-activate-code", response_model=ConfirmCodeSchema)
+async def handle_generate_confirm_code(
+    email_service: email_service_dep,
+    payload: Annotated[PayloadSchema, Depends(deps.payload_dependency)],
+):
+    return await email_service.send_activation_code(payload)
+
+
+@router.patch("/activate", response_model=AccessTokenSchema)
+async def handle_activate_user(
+    service: service_dep,
+    payload: Annotated[PayloadSchema, Depends(deps.payload_dependency)],
+):
+    return await service.activate_user(payload)
