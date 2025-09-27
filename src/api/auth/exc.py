02edc12b76472ab477ed406d03a313fccea9c697
Maxim Danilov <abl1v1on@yandex.ru>
2025-09-27 15:58:56 +0500

    create user already activated exc

--- src/api/auth/exc.py
+++ src/api/auth/exc.py
@@ -33,3 +33,11 @@ class InvalidTokenTypeException(HTTPException):
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail=f"token type must be {token_type.value}",
         )
+
+
+class UserAlreadyActivatedException(HTTPException):
+    def __init__(self) -> None:
+        super().__init__(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=f"user already activated",
+        )
